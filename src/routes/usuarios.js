fdf000521fbadb7edc249962b7af0556925109d8
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-21 00:17:03 -0300

    feat: route para atualizar a imagem e buscar imagem do perfil

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,13 +3,22 @@ var router = express.Router();
 
 var usuarioController = require("../controllers/usuarioController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+// Cadastro e autenticação de usuário
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
 router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
+});
+
+// Foto de perfil do usuário
+router.put("/atualizarImagemPerfil", function (req, res) {
+    usuarioController.atualizarImagemPerfil(req, res);
+});
+
+router.post("/retornarImagemPerfil", function (req, res) {
+    usuarioController.retornarImagemPerfil(req, res);
 });
 
 module.exports = router;

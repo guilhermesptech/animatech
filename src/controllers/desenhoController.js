5e2ac8916fd70f26703a74de980da05619c9f50b
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-29 13:57:19 -0300

    feat: route direciona a requisição para esse controller, que vai chamar a model para realizar a consulta no banco de dados

--- src/controllers/desenhoController.js
+++ src/controllers/desenhoController.js
@@ -2,15 +2,27 @@ var desenhoModel = require("../models/desenhoModel");
 
 function top3(req, res) {
     desenhoModel.top3()
-    .then(function (resultado) {
-        res.json(resultado);
-    })
-    .catch(function (erro) {
-        console.log("Erro ao buscar o top 3", erro);
-        res.json({erro: "Erro na busca"});
-    })
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.log("Erro ao buscar o top 3", erro);
+            res.json({ erro: "Erro na busca" });
+        })
+}
+
+function generosMaisCurtidos(req, res) {
+    desenhoModel.generosMaisCurtidos()
+        .then(function (resultado) {
+            res.json(resultado);
+        })
+        .catch(function (erro) {
+            console.log("Erro ao buscar os gêneros mais curtidos", erro);
+            res.json({ erro: "Erro na busca" });
+        })
 }
 
 module.exports = {
-    top3
+    top3,
+    generosMaisCurtidos
 }

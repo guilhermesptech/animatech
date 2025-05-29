5ebf7b981b32affe39cdf0dec53731e907e21fd2
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-29 13:57:52 -0300

    feat: consulta no banco de dados os generos mais curtidos pelo usuário

--- src/models/desenhoModel.js
+++ src/models/desenhoModel.js
@@ -18,6 +18,28 @@ function top3() {
     return database.executar(instrucaoSql);
 }
 
+function generosMaisCurtidos() {
+    const instrucaoSql = `
+        SELECT 
+	        g.nome,
+            g.descricao,
+            g.imagem_url as 'imagem_url',
+            SUM(d.curtidas) as 'curtidas_genero'
+        FROM
+	        genero g
+        INNER JOIN
+	        desenho d on g.id = d.fkgenero
+        GROUP BY
+	        g.nome, g.descricao, g.imagem_url
+        ORDER BY
+	        curtidas_genero DESC;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    top3
+    top3,
+    generosMaisCurtidos
 };

6c835423832a19ca5829cbabb0e1968b2938becc
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-29 13:54:34 -0300

    correção na model de usuario, genero nao estava sendo definido

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,7 +10,7 @@ function autenticar(usuario, email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, sobrenome, dataNascimento, usuario, email, senha) {
+function cadastrar(nome, sobrenome, dataNascimento, genero, usuario, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, dataNascimento, genero, usuario, email, senha);
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores

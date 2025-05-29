25231784fe14ab0e9dd43dcc776b79a72992792e
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-29 13:56:07 -0300

    feat: route para buscar generos mais curtidos pelo usuário

--- src/routes/desenhos.js
+++ src/routes/desenhos.js
@@ -7,4 +7,8 @@ router.get("/top3", function (req, res) {
     desenhoController.top3(req, res);
 });
 
+router.get("/generosMaisCurtidos", function (req, res) {
+    desenhoController.generosMaisCurtidos(req, res);
+})
+
 module.exports = router;

5f7bd7ee20eb28700396b23fdd2fc973133fdf94
guilhermesptech <guilherme.doliveira@sptech.school>
2025-05-29 23:27:32 -0300

    feat: adicionando nome de usuario na sessao

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -3,11 +3,14 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME;
     var sobrenome = sessionStorage.SOBRENOME;
+    var nome_usuario = sessionStorage.NOME_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
+    var n_usuario = document.getElementById("n_usuario");
 
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome + " " + sobrenome;
+        n_usuario.innerHTML = nome_usuario;
     } else {
         window.location = "../login.html";
     }

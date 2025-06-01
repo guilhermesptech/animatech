1430446b4329bf42e878ee1fac874f100e150b56
guilhermesptech <guilherme.doliveira@sptech.school>
2025-06-01 19:54:45 -0300

    refactor: método para salvar voto no banco de dados verifica primeiro se já existe o registo na tabela, se sim faz apenas um update para não inflar a tabela, se não, faz o insert

--- src/models/desenhoModel.js
+++ src/models/desenhoModel.js
@@ -1,75 +1,142 @@
 var database = require("../database/config")
 
-function top3() {
+function exibirPodioDoUsuario(usuarioId) {
     const instrucaoSql = `
-        SELECT
-            nome,
-            descricao,
-            imagem_url,
-            curtidas
-        FROM
-            desenho
+        SELECT 
+            d.nome AS nome_desenho,
+            d.descricao,
+            d.imagem_url,
+            v.quantidade_votos
+        FROM 
+	        voto v
+        INNER JOIN 
+	        desenho d ON v.fkdesenho = d.id
+        INNER JOIN 
+	        usuario u ON v.fkusuario = u.id
+        WHERE
+	        v.fkusuario = ${usuarioId}
         ORDER BY 
-            curtidas DESC
-        LIMIT 3
+            v.quantidade_votos DESC
+        LIMIT 
+            3;
     `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL (exibir pódio do usuário): \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function generosMaisCurtidos() {
+function exibirOsGenerosPreferidosDoUsuario(usuarioId) {
     const instrucaoSql = `
         SELECT 
 	        g.nome,
-            g.descricao,
-            SUM(d.curtidas) as 'curtidas_genero'
+            SUM(v.quantidade_votos) as 'total_votos'
         FROM
-	        genero g
+	        voto v
         INNER JOIN
-	        desenho d on g.id = d.fkgenero
+	        desenho d ON d.id = v.fkdesenho
+        INNER JOIN
+            genero g ON d.fkgenero = g.id
+        WHERE
+            v.fkusuario = ${usuarioId}
         GROUP BY
-	        g.nome, g.descricao
+	        g.nome
         ORDER BY
-	        curtidas_genero DESC;
+	        total_votos DESC
+        LIMIT 
+            5;
     `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL (exibir os gêneros preferidos do usuário): \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function listarDesenhos() {
+function exibirTodosOsDesenhos() {
     const instrucaoSql = `
-    SELECT
-        id,
-        nome,
-        descricao,
-        imagem_url
-    FROM
-        desenho;
+        SELECT
+            id,
+            nome,
+            descricao,
+            imagem_url
+        FROM
+            desenho;
     `;
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL (exibir todos os desenhos): \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function votar(id) {
+function votar(usuarioId, desenhoId) {
+
+    const verificaSql = `
+        SELECT 
+            * 
+        FROM 
+            voto
+        WHERE
+            fkusuario = ${usuarioId}
+        AND
+            fkdesenho = ${desenhoId};
+    `;
+
+    console.log("Verificando se já existe o registro na tabela: \n" + verificaSql);
+    return database.executar(verificaSql)
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+
+                const instrucaoSql = `
+                    UPDATE
+                        voto
+                    SET
+                        quantidade_votos = quantidade_votos + 1
+                    WHERE
+                        fkusuario = ${usuarioId}
+                    AND
+                        fkdesenho = ${desenhoId};
+                `;
+                console.log("Executando a instrução SQL (update na tabela voto): \n" + instrucaoSql)
+                return database.executar(instrucaoSql);
+            } else {
+                const instrucaoSql = `
+                    INSERT INTO voto
+                    (
+                        quantidade_votos,
+                        fkusuario,
+                        fkdesenho
+                    )
+                    VALUES
+                    (
+                        1,
+                        ${usuarioId},
+                        ${desenhoId}
+                    );
+                `;
+                console.log("Executando a instrução SQL (insert na tabela voto): \n" + instrucaoSql)
+                return database.executar(instrucaoSql);
+            }
+        })
+
+
+
     const instrucaoSql = `
-    UPDATE
-        desenho
-    SET
-        curtidas = curtidas + 1
-    WHERE
-        id = ${id}
-    `
+
+        UPDATE
+            voto
+        SET
+            quantidade_votos = quantidade_votos + 1
+        WHERE
+            fkusuario = ${usuarioId}
+        AND
+            fkdesenho = ${desenhoId};
+
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-    top3,
-    generosMaisCurtidos,
-    listarDesenhos,
+    exibirPodioDoUsuario,
+    exibirOsGenerosPreferidosDoUsuario,
+    exibirTodosOsDesenhos,
     votar
 };

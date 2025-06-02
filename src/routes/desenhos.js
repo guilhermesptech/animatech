7172a1596e10a058b65d11f0a8095ad300e9440c
guilhermesptech <guilherme.doliveira@sptech.school>
2025-06-02 19:32:21 -0300

    feat: mapeando novas rotas GET para /exibirPodioGeral e /exibirOsGenerosPreferidosNoGeral

--- src/routes/desenhos.js
+++ src/routes/desenhos.js
@@ -7,13 +7,21 @@ router.get("/exibirPodioDoUsuario/:usuarioId", function (req, res) {
     desenhoController.exibirPodioDoUsuario(req, res);
 });
 
+router.get("/exibirPodioGeral", function (req, res) {
+    desenhoController.exibirPodioGeral(req, res);
+})
+
 router.get("/exibirOsGenerosPreferidosDoUsuario/:usuarioId", function (req, res) {
     desenhoController.exibirOsGenerosPreferidosDoUsuario(req, res);
 })
 
+router.get("/exibirOsGenerosPreferidosNoGeral", function (req, res) {
+    desenhoController.exibirOsGenerosPreferidosNoGeral(req, res);
+})
+
 router.get("/exibirTodosOsDesenhos", function (req, res) {
     desenhoController.exibirTodosOsDesenhos(req, res);
-}) 
+})
 
 router.post("/votar", function (req, res) {
     desenhoController.votar(req, res);

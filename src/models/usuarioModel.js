c6506f89357df53ea4d3b072a86f14e83a40c98b
guilhermesptech <guilherme.doliveira@sptech.school>
2025-06-02 21:57:27 -0300

    refactor: refatorando model para buscar de foto do usuário no banco de dados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -22,22 +22,14 @@ function cadastrar(nome, sobrenome, dataNascimento, genero, usuario, email, senh
     return database.executar(instrucaoSql);
 }
 
-function atualizarImagemPerfil(imagem_url, id) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function atualizarImagemPerfil():", imagem_url, id);
-
+function exibirFotoDoUsuario(usuarioId) {
     var instrucaoSql = `
-        UPDATE usuario SET imagem_url = '${imagem_url}' WHERE id = '${id}';
-    `;
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-function retornarImagemPerfil(id) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function retornarImagemPerfil():", id);
-
-    var instrucaoSql = `
-        SELECT imagem_url FROM usuario WHERE id = '${id}';
+        SELECT 
+            imagem_url 
+        FROM 
+            usuario 
+        WHERE 
+            id = '${usuarioId}';
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -55,7 +47,7 @@ function salvarDataHoraAcesso(id) {
             ${id}
         );
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    console.log("Executando a instrução SQL (exibir a foto do usuário): \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
@@ -81,8 +73,7 @@ function exibirMesMaisAcessadoPeloUsuario(usuarioId) {
 module.exports = {
     autenticar,
     cadastrar,
-    atualizarImagemPerfil,
-    retornarImagemPerfil,
+    exibirFotoDoUsuario,
     salvarDataHoraAcesso,
     exibirMesMaisAcessadoPeloUsuario
 };
